75e9e6f13758f5406261de9f6a01fe070a284fe1
Matiasalbina <154567225+Matiasalbina@users.noreply.github.com>
2024-03-15 15:13:19 -0300

    primer commit "Funcion expresion y Arrow"

---
+++ 1_funcion.js
@@ -0,0 +1,5 @@
+suma = function(a, b, c) {
+    return(a + b + c)
+}
+let resultado = suma(5, 10, 15)
+console.log(resultado)
---
+++ 2_arrows.js
@@ -0,0 +1,9 @@
+/*----------Funcion Clasica---------*/
+let suma = function(a, b){
+    return a + b
+    }
+   
+    
+
+/*--------Funcion Arrow---------*/
+suma = (a, b) => 5 + 2
